4bca38dc1c0596b43c6945ff2dda79b34567d25b
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-11 22:28:56 +0200

    HeroCard

---
+++ src/modules/heroes/components/HeroeCard.jsx
@@ -0,0 +1,39 @@
+import  { Link } from "react-router-dom";
+
+export const HeroeCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const heroImageUrn = `/assets/heroes/${id}.jpg`;
+
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img className="card-img" src={heroImageUrn} alt={superhero} />
+          </div>
+          <div className="col-8">
+            <div className="card-body">
+              <div className="card-title">{superhero}</div>
+
+              <p className="card-text">{alter_ego}</p>
+
+              {alter_ego !== characters && <p>{characters}</p>}
+
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>More...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/modules/heroes/components/HeroesList.jsx
+++ src/modules/heroes/components/HeroesList.jsx
@@ -1,14 +1,14 @@
+import { HeroeCard } from "./HeroeCard";
 import { getHeroesByPublisher } from "../helpers";
 
-export const HeroesList = ({publisher}) => {
+export const HeroesList = ({ publisher }) => {
+  const heroes = getHeroesByPublisher(publisher);
 
-    const heroes = getHeroesByPublisher(publisher)
-
-    return (
-        <ul>
-            {heroes.map(({id, superhero}) => (
-                <li key={id}>{superhero}</li>
-            ))}
-        </ul>
-    );
-}
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroeCard key={hero.id} {...hero} />
+      ))}
+    </div>
+  );
+};
