5b66e630d3190df45dedbce951466eb25706eb48
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-11 20:18:22 +0200

    Subroutes files to hide navbar from login

---
+++ src/modules/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,22 @@
+import { Navbar } from "../../ui";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { DcPage, MarvelPage, SearchPage, HeroPage } from "../pages";
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="dc" element={<DcPage />} />
+          <Route path="marvel" element={<MarvelPage />} />
+
+          <Route path="search" element={<SearchPage />} />
+          <Route path="hero" element={<HeroPage />} />
+
+          <Route path="/" element={<Navigate to={"marvel"} />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { MarvelPage } from "../modules/heroes";
-import { DcPage } from "../modules/heroes";
+import { HeroesRoutes } from "../modules/heroes/routes/HeroesRoutes";
 import { LoginPage } from "../modules/auth";
 import { Navbar } from "../modules/ui";
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="dc" element={<DcPage />} />
-        <Route path="marvel" element={<MarvelPage />} />
-
-        <Route path="/" element={<Navigate to={"login"} />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={<HeroesRoutes />}/>
       </Routes>
     </>
   );
