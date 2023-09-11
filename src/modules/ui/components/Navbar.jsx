a22b21478e46837ab22c7c86918f4e7acb58a031
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-11 20:28:17 +0200

    useNavigate + replace

--- src/modules/ui/components/Navbar.jsx
+++ src/modules/ui/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => navigate("/login", { replace: true});
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -9,15 +13,30 @@ export const Navbar = () => {
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
-          <NavLink className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ""}`} to="/marvel">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/marvel"
+          >
             Marvel
           </NavLink>
 
-          <NavLink className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ""}`} to="/dc">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/dc"
+          >
             DC
           </NavLink>
 
-          <NavLink className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ""}`} to="/search">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/search"
+          >
             Search
           </NavLink>
         </div>
@@ -25,8 +44,8 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 justify-content-end">
         <ul className="navbar-nav ml-auto">
-            <span className="nav-item nav-link text-primary">Gerard</span>
-          <button className="nav-item nav-link">
+          <span className="nav-item nav-link text-primary">Gerard</span>
+          <button className="nav-item nav-link" onClick={onLogout}>
             Logout
           </button>
         </ul>
