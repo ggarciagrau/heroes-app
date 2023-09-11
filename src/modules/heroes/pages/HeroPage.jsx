1d4c1da8b2fcc3b799ae9bbae25d4e52d68a7e44
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-11 22:41:34 +0200

    Component redirect - Hero detail

--- src/modules/heroes/pages/HeroPage.jsx
+++ src/modules/heroes/pages/HeroPage.jsx
@@ -1,6 +1,18 @@
+import { useParams, Navigate } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
 
+    const {id} = useParams();
+
+    const hero = getHeroById(id);
+
+    if (!hero) {
+        return <Navigate to="/" />
+    }
+
     return (
-        <>HeroPage</>
+        <>{hero.superhero}</>
     );
+
 }
