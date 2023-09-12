7ee45694b86d1031deb2303f559adde83e54e6b9
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-12 21:43:27 +0200

    Clean

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,6 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HeroesRoutes } from "../modules/heroes/routes/HeroesRoutes";
 import { LoginPage } from "../modules/auth";
-import { Navbar } from "../modules/ui";
 
 export const AppRouter = () => {
   return (
