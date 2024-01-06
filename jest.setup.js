e98e45081408ad5d828a580bd5df7564b2a026cd
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-06 18:54:07 +0100

    JSX-Transform for Jest (avoid importing react for component testing)

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    moduleNameMapper: {
-      // Mapeo para tus importaciones de estilos y assets, si es necesario
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-    },
-  };
-  
